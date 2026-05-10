62456f40bf286d769ca785e558379b43bc4db69c
Carrielti <lucasgccardosoti@gmail.com>
2026-05-10 01:17:44 -0300

    Move arquivos do site para raiz

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/* APRENDIZADO - Conexão com o banco de dados
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "eco_pilhas";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM pontos";
+$result = $conn->query($sql);
+*/
+?>
